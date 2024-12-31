6757d4505acc43203a0a177bd5da27f3562a5de3
Ilya Avtonomov <laymed@mail.ru>
2024-12-31 20:12:47 +0300

    HELLO WORLD!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld beanHello1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHello1.getMessage());
+
+        HelloWorld beanHello2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHello2.getMessage());
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        beanCat1.meow();
+
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        beanCat2.meow();
+
+        System.out.println(beanHello1.equals(beanHello2));
+        System.out.println(beanCat1.equals(beanCat2));
     }
 }
